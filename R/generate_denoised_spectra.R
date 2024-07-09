0c4e552c986fe673f9c80b78182f956db15e5be0
banerjeeshayantan <banerjeeshayantan@gmail.com>
2024-07-09 13:22:19 +0530

    denoising

--- R/generate_denoised_spectra.R
+++ R/generate_denoised_spectra.R
@@ -3,12 +3,13 @@
 #' Takes in a list of individual spectrum with fragments labeled with frequencies, denoises it using fixed cutoff and writes the resulting spectrum into a file
 #' @param aggregate_list: output from label_individual_spectrum()
 #' @param folder_path: same path as the one used in preprocess()
+#' @param ion_mode: ionization mode = "pos" or "neg" (default = "pos")
 #' @param custom_threshold: must be between 0 and 1 (default = NULL). If the user wants to apply a threshold different from ours (0.12 (95%CI- 0.08-0.15)) they can do so using this parameter. The threshold when multiplied by the number of replicate spectra (for a given feature) will tell us that only fragments with frequencies above this value will be considered as signal fragments and retained in the spectrum
 #' @return directly prints the spectra to the file in .txt and .mzML format
 #' @examples
 #' # Example usage of the function
 #' generate_denoised_spectra(l3, folder_path, NULL)
-generate_denoised_spectra <- function(aggregate_list, folder_path, custom_threshold = NULL){
+generate_denoised_spectra <- function(aggregate_list, folder_path, custom_threshold = NULL, ion_mode = "pos"){
   if (!dir.exists(file.path(folder_path, "Denoised_spectra"))) {
     dir.create(file.path(folder_path, "Denoised_spectra/"))
   }
@@ -31,13 +32,34 @@ generate_denoised_spectra <- function(aggregate_list, folder_path, custom_thresh
       threshold = 0.12
     }
     #file_names = paste(names(freq_df)[[j]], names(freq_df[[j]]), sep="/")
-    file_names = names(freq_df[[j]])
+    file_names = names(freq_df[[j]]);sps_tic_20_all_freq = list(); mz = list(); inten = list()
+
 
     for(k in 1:length(freq_df[[j]])){
       f = freq_df[[j]][[k]]
       sps_tic_20_all_freq = tune_parameters(f, threshold, file_names[k], path)
-
+      mz[[k]] = sps_tic_20_all_freq[[1]]
+      inten[[k]] = sps_tic_20_all_freq[[2]]
     }
+    if(ion_mode == "pos"){
+      pol = 1L
+    }else{
+      pol = 0L
+    }
+    spd <- DataFrame(
+      msLevel = c(rep(2L, length(mz))),
+      polarity = c(rep(pol, length(mz))),
+      # id = c(names(freq_df)[j]),
+      name = c(names(freq_df)[j]))
+
+    spd$mz<- mz
+    spd$intensity <- inten
+    sps <- Spectra(spd)
+
+    fl = paste(path, names(freq_df)[j],".mzML",sep="")
+    export(sps, MsBackendMzR(), file = fl)
+
+
   }
 
 }
--- R/tune_parameters.R
+++ R/tune_parameters.R
@@ -5,7 +5,7 @@
 #' @param threshold: see function description for generate_denoised_spectra
 #' @param name: names of the spectra such as "scan_1.txt" "scan_2.txt" and so on
 #' @param path: folder path to store the denoised spectra in .txt and .mzML format
-#' @return directly prints the spectra to the file in .txt and .mzML format
+#' @return directly prints the spectra to the file in .txt format and returns the spectrum-wise mz and intensity
 #' @examples
 #' # Example usage of the function
 #' tune_parameters(agg_df, threshold, "scan_1.txt", path)
@@ -13,23 +13,25 @@ tune_parameters <- function(agg_df, threshold, name, path){
   colnames(agg_df)[2:4] = c("Mean_MZ", "Mean_Intensity", "Frequency")
   agg_df_cutoff = subset(agg_df, agg_df$Frequency >= threshold)
   agg_df_cutoff = agg_df_cutoff[order(agg_df_cutoff$Mean_MZ, decreasing = F),]
-  #creating the composite spectra
-  spd <- DataFrame(
-    msLevel = c(2L),
-    polarity = c(1L),
-    id = c(name),
-    name = c(name))
-  spd$mz <- list(
-    c(agg_df_cutoff$Mean_MZ))
-  spd$intensity <- list(
-    c(agg_df_cutoff$Mean_Intensity))
-  sps <- Spectra(spd)
+  # #creating the composite spectra
+  # spd <- DataFrame(
+  #   msLevel = c(2L),
+  #   polarity = c(1L),
+  #   id = c(name),
+  #   name = c(name))
+  # spd$mz <- list(
+  #   c(agg_df_cutoff$Mean_MZ))
+  # spd$intensity <- list(
+  #   c(agg_df_cutoff$Mean_Intensity))
+  # sps <- Spectra(spd)
 
   #plotSpectra(sps, main = paste(sps$name, " (Freq_cutoff_", as.character(threshold),")", sep=""))
   # folder_create = dir.create(paste("~/metabolomics/WTC+DIELdata/results_ppm=5/Denoised_spectra/",name,"/", sep=""))
   #setwd(paste("~/metabolomics/Sportomics/Database_metabolites/Aggregate_top_5_files_SS_score/Files_of_interest_Sportomic/After_filtering_stage_2_spectra/", name,sep=""))
   name_exp= paste(path, name, sep="")
-  sps_df=data.frame(fragments = peaksData(sps)[[1]][,1], intensity =  peaksData(sps)[[1]][,2])
+  #sps_df=data.frame(fragments = peaksData(sps)[[1]][,1], intensity =  peaksData(sps)[[1]][,2])
+  sps_df = data.frame(fragments = agg_df_cutoff$Mean_MZ, intensity = agg_df_cutoff$Mean_Intensity)
   write.table(sps_df, name_exp, sep="\t", col.names = TRUE, row.names = FALSE, quote = FALSE)
-  return(sps)
+
+  return(list(agg_df_cutoff$Mean_MZ, agg_df_cutoff$Mean_Intensity))
 }
