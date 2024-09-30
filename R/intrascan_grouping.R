6212c46abf04463337b40a9cd5581fff685e69c1
banerjeeshayantan <banerjeeshayantan@gmail.com>
2024-09-30 14:54:39 +0530

    Update tutorial

--- R/intrascan_grouping.R
+++ R/intrascan_grouping.R
@@ -14,18 +14,18 @@ intrascan_grouping <- function(scan, tol) {
   if (length(scan[,1]) == 0) {
     return(NULL)
   } else {
-    scan <- scan[order(scan[,1], decreasing = TRUE), ]
+    scan <- scan[order(scan[,1]), ]
     scan$unique_id <- paste("frag", scan[,1], sep = "_")
 
     scan <- scan %>%
       dplyr::arrange(fragments) %>%
-      dplyr::mutate(group = cumsum(c(1, diff(fragments) > tol)))
+      dplyr::mutate(group = cumsum(c(1, diff(fragments) >= tol)))
 
     agg_df <- aggregate(cbind(fragments = scan$fragments, intensity = scan$intensity),
                         by = list(group = scan$group),
                         FUN = function(x) c(mean = mean(x), sum = sum(x)))
 
-    new_scan <- data.frame(fragments = agg_df$fragments[, "mean"], intensity = agg_df$intensity[, "sum"])
+    new_scan <- data.frame(fragments = agg_df[, 2][,1], intensity = agg_df[, 3][,2]) #print the agg_df dataframe to look for the second and fifth columns. Second column contain mean mz and fifth contain sum intensities
     new_scan <- new_scan[new_scan$intensity != 0, ]  # remove fragments with zero intensities
 
     return(new_scan)
